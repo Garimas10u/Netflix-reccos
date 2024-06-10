5e6e922708269b2c86e3acd1aec4fee93bedc074
Garima Singh <garimasingh10u@gmail.com>
2024-06-11 02:02:33 +0530

    updated files

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -9,7 +9,7 @@ const VideoBackground = ({movieId}) => {
   return (
    <div className="w-screen">
         <iframe className='w-full aspect-video'    
-         src={ "https://www.youtube.com/embed/"+trailerVideo?. key + "?&autoplay=1&mute=1&loop=1" }
+         src={"https://www.youtube.com/embed/"+trailerVideo?.key +"?&autoplay=1&mute=1&loop=1"}
           title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share " referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>  
    </div>
   )
